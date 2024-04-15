0d89f960f9a32628827ff1c6d1dcc2ad07c77ff2
ckinsing <156004110+ckinsing@users.noreply.github.com>
2024-04-15 15:17:17 -0400

    Update finalcode.py

--- finalcode.py
+++ finalcode.py
@@ -1,1 +1,12 @@
+print('Welcome to the Avneger's Personality Quiz. This quiz will tell you what Avenger you are from the Marvel Cinematic Universe.')
+print('Please answer the following five questions to get your result using A, B, C, D, or E as your answer choices.')
 
+questions = ('What superpower would you want to have?: ', 'Others would describe you as ______?: ', '', '', '', '')
+
+options = ((), (), (), (), ())
+
+score = 0
+qnumber = 0
+
+
+q1 = 
